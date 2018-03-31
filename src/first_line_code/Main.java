70cd56b604b1a0258429f9775a419b3c4befe463
maodo <maodosw@gmail.com>
2018-03-31 11:52:21 +0000

    initial commit

---
+++ src/first_line_code/Main.java
@@ -0,0 +1,11 @@
+package first_line_code;
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		System.out.print("Bonjour\n");
+		System.out.println("welcome");
+
+	}
+
+}
